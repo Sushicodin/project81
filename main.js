62bbb98f46cdeacbff6127ca572209d51e8f023d
Sushicodin <87022921+Sushicodin@users.noreply.github.com>
2021-11-13 17:22:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+canvas=document.getElementById("canvasCoolness");
+ctx=canvas.getContext("2d");
+
+
+ctx.beginPath();
+ctx.strokeStyle="gray";
+ctx.lineWidth=2;
+ctx.rect(230, 100, 250, 250, 0 *Math.PI);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=2;
+ctx.arc(250, 210, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=2;
+ctx.arc(350, 210, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=2;
+ctx.arc(450, 210, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=2;
+ctx.arc(300, 250, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth=2;
+ctx.arc(400, 250, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+
